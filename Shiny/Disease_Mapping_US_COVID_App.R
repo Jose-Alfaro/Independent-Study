d008b28610dbfe08fe8e2c6b29469b30ae4b630d
Jose-Alfaro <43125011+Jose-Alfaro@users.noreply.github.com>
2020-09-27 17:02:36 -0500

    Uploaded Shiny App

---
+++ Shiny/Disease_Mapping_US_COVID_App.R
@@ -0,0 +1,132 @@
+library(ggplot2)
+library(shapefiles)
+library(sp)
+library(CARBayes)
+library(leaflet)
+library(rgdal)
+library(leaflet)
+library(shiny)
+
+## Load data from Github directly and prepare `tmp`
+dta <- read.csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"))
+names(dta) <- c("UID", "iso2", "iso3", "code3", "FIPS", "Admin2",
+                "Province_State", "Country_Region", "lat", "long", "Combined_key",
+                as.character(as.Date(12:ncol(dta) - 12, origin = "2020/01/22")))
+dta <- subset(dta, !is.na(FIPS))
+dta$FIPS <- sprintf("%05d", dta$FIPS)
+excl <- c("Alaska", "Hawaii", "American Samoa","Diamond Princess", "Grand Princess", "Guam", "Northern Mariana Islands", 
+          "Puerto Rico", "Virgin Islands")
+tmp <- subset(dta, !Province_State %in% excl & lat > 0)
+tmp$UID <- tmp$iso2 <- tmp$iso3 <- tmp$code3 <- tmp$Country_Region <- tmp$Combined_key <- NULL
+row.names(tmp) <- tmp$FIPS
+
+## Data set up for county population
+county <- read.csv("../Data/PopulationEstimates.csv")
+county <- subset(county, select = c(1, 20))
+colnames(county) <- c("FIPS", "Pop_Estimate_2019") 
+county$FIPS <- sprintf("%05d", county$FIPS)
+county$Pop_Estimate_2019 <- as.numeric(gsub(",", "", county$Pop_Estimate_2019))
+county <- merge(county, tmp, by = "FIPS") ## dim 3108 255
+county[, 7:ncol(county)] <- county[, 7:ncol(county)]/county[, 2] * 100
+county$Pop_Estimate_2019 <- NULL
+row.names(county) <- county$FIPS 
+
+## User Date Specification 
+selectdates2 <- function(data, start, end){
+    keep <- data[, 1:5]
+    data <- data[, -c(1:5)]
+    v1 <- as.Date(names(data))
+    v2 <- which(v1 >= as.Date(start) & v1 <= as.Date(end))
+    tdta <- data[, v2]
+    tdta$Sum <- rowSums(tdta)
+    dta2 <- cbind(keep, tdta)
+    return(dta2)
+}
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("United States COVID-19 Mapping"),
+    tags$em("By: Jose Alfaro"),
+    tags$hr(),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            dateRangeInput("daterange", "Date Range:",
+                           start = as.character(Sys.Date() - 6),
+                           end = as.character(Sys.Date())),
+            actionButton("submitButton", "Submit", class = "btn btn-primary"),
+            textOutput("dateCheck")
+        ),
+        
+        # Display leaflet plot of cases
+        mainPanel(
+            leafletOutput("leafletPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    output$leafletPlot <- renderLeaflet({
+    
+        ## Insert submitButton
+        input$submitButton
+        
+        isolate(dta2 <- selectdates2(data = tmp, start = input$daterange[1], end = input$daterange[2]))
+        row.names(dta2) <- dta2$FIPS
+        
+        ## Loads SHP and DBF File
+        covidshp <- read.shp("../Shape_Files/cb_2018_us_county_500k.shp")
+        coviddbf <- read.dbf("../Shape_Files/cb_2018_us_county_500k.dbf")
+        
+        coviddbf$dbf <- data.frame(FIBS = with(coviddbf$dbf, paste0(STATEFP, COUNTYFP)), coviddbf$dbf)
+        
+        ## Confirmed cases in the last 7 days
+        covid.sp <- combine.data.shapefile(data = dta2, shp = covidshp, dbf = coviddbf)
+        proj4string(covid.sp) <- CRS("+proj=longlat +datum=WGS84 +no_defs")
+        covid.sp <- spTransform(covid.sp, CRS("+proj=longlat +datum=WGS84 +no_defs"))
+        
+        ## Format popup data for leaflet map.
+        pop_num <- prettyNum(covid.sp$Sum, big.mark = ',', preserve.width = "none")
+        popup_dat <- paste0("<strong>County: </strong>", 
+                            covid.sp$Admin2, 
+                            "<br><strong>Value: </strong>", 
+                            pop_num)
+        
+        colours <- colorNumeric(palette = "YlOrRd", domain = covid.sp@data$Sum)
+        
+        county <- leaflet(covid.sp) %>%
+            addTiles() %>%
+            addPolygons(
+                fillColor = ~ colours(Sum),
+                weight = 1,
+                opacity = 0.7,
+                color = "white",
+                dashArray = '3',
+                fillOpacity = 0.7,
+                highlight = highlightOptions(
+                    weight = 5,
+                    color = "#666",
+                    dashArray = "",
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE
+                ),
+                popup = popup_dat
+            ) %>%
+            addLegend(
+                pal = colours,
+                values = covid.sp@data$Sum,
+                opacity = 1,
+                title = "Count"
+            ) %>%
+            addScaleBar(position = "bottomleft")
+    })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
